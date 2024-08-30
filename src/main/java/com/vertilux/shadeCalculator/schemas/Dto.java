77a44f270ac8222941cabe924a4efe27955f748e
frank <franklin.neves.filho@gmail.com>
2024-08-30 12:13:30 -0400

    created DTO class for requests to be dynamic

---
+++ src/main/java/com/vertilux/shadeCalculator/schemas/Dto.java
@@ -0,0 +1,11 @@
+package com.vertilux.shadeCalculator.schemas;
+
+/**
+ * <h1>Dto</h1>
+ *
+ * This interface is used to unify all dto classes.
+ * This ensures that the controller can receive any dto object.
+ *
+ * @author Franklin Neves
+ */
+public interface Dto {}
