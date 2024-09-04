03a3779429f82531b4ee97d366785b16404c8373
frank <franklin.neves.filho@gmail.com>
2024-09-04 16:01:44 -0400

    Dynamic component Controller

---
+++ src/main/java/com/vertilux/shadeCalculator/controllers/ComponentController.java
@@ -0,0 +1,87 @@
+package com.vertilux.shadeCalculator.controllers;
+
+import com.vertilux.shadeCalculator.models.Response;
+import com.vertilux.shadeCalculator.schemas.RollerFabricCreation;
+import com.vertilux.shadeCalculator.schemas.RollerTubeCreation;
+import com.vertilux.shadeCalculator.schemas.Schema;
+import com.vertilux.shadeCalculator.services.RollerFabricService;
+import com.vertilux.shadeCalculator.services.RollerTubeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * <h1>ItemController</h1>
+ *
+ * This class handles all the crud operations for the items in the system.
+ * including:
+ * - RollerTubes
+ * - Fabrics
+ * - Components
+ */
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/component")
+public class ComponentController extends MainController {
+    private RollerTubeService rollerTubeService;
+    private RollerFabricService fabricService;
+
+    private final Supplier<Response> getAllTubes = () -> rollerTubeService.getAllRollerTubes();
+    private final Function<String, Response> getTubeByName = (name) -> rollerTubeService.getRollerTubeByName(name);
+    private final Function<String, Response> getTubeById = (id) -> rollerTubeService.getRollerTubeById(id);
+    private final Function<Schema, Response> saveTube = (tube) -> rollerTubeService.createRollerTube((RollerTubeCreation) tube);
+    private final Function<String, Response> deleteTube = (id) -> rollerTubeService.deleteRollerTube(id);
+    private final BiFunction<String, Schema, Response> updateTube = (id, tube) -> rollerTubeService.updateRollerTube(id, (RollerTubeCreation) tube);
+
+    private final Supplier<Response> getAllFabrics = () -> fabricService.getAllRollerFabrics();
+    private final Function<Schema, Response> saveFabric = (fabric) -> fabricService.createRollerFabric((RollerFabricCreation) fabric);
+
+
+    @PostMapping("/{serviceName}/save")
+    public ResponseEntity<Response> save(@PathVariable("serviceName") String serviceName, @RequestBody Schema object){
+        return switch (serviceName) {
+            case "tube" -> request(saveTube, object);
+            case "fabric" -> request(saveFabric, object);
+            default -> factory.createBadRequestResponse();
+        };
+    }
+
+    @GetMapping("/{serviceName}/get-all")
+    public ResponseEntity<Response> getAll(@PathVariable("serviceName") String serviceName){
+        return switch (serviceName) {
+            case "tube" -> getAll(getAllTubes);
+            case "fabric" -> getAll(getAllFabrics);
+            default -> factory.createBadRequestResponse();
+        };
+    }
+
+    @GetMapping("/{serviceName}/get-by-name")
+    public ResponseEntity<Response> getByName(@PathVariable("serviceName") String serviceName, @RequestParam String name){
+        return switch (serviceName) {
+            case "tube" -> getByParam(getTubeByName, name);
+            default -> factory.createBadRequestResponse();
+        };
+    }
+
+    @GetMapping("/{serviceName}/get-by-id")
+    public ResponseEntity<Response> getById(@PathVariable("serviceName") String serviceName, @RequestParam String id){
+        return switch (serviceName) {
+            case "tube" -> getByParam(getTubeById, id);
+            default -> factory.createBadRequestResponse();
+        };
+    }
+
+    @DeleteMapping("/{serviceName}/delete")
+    public ResponseEntity<Response> delete(@PathVariable("serviceName") String serviceName, @RequestParam String id){
+        return switch (serviceName) {
+            case "tube" -> getByParam(deleteTube, id);
+            default -> factory.createBadRequestResponse();
+        };
+    }
+
+}
